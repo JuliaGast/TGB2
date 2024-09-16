5e42287b97712bae4424bef23cbb7a0177e21440
Shenyang Huang <shenyang.huang@mail.mcgill.ca>
2024-09-16 15:08:20 -0400

    adding enron and uci scripts

--- tgb/datasets/tgbl_uci/tgbl-uci.py
+++ tgb/datasets/tgbl_uci/tgbl-uci.py
@@ -30,15 +30,35 @@ def write2csv(outname, out_dict):
             writer.writerow(row)
 
 
+def load_write_csv(fname, outname):
+    """
+    load the raw csv file
+    """
+    num_lines = 0
+    with open(fname, 'r') as f:
+        with open(outname, 'w') as out_f:
+            writer = csv.writer(out_f, delimiter =',')
+            reader = csv.reader(f, delimiter =',')
+            writer.writerow(['source','destination','timestamp'])
+            for row in reader: 
+                if (num_lines == 0):
+                    num_lines += 1
+                    continue
+                head = int(row[1])
+                tail = int(row[2])
+                ts = int(row[3])
+                row = [head, tail, ts]
+                writer.writerow(row)
+                num_lines += 1
+    return num_lines
+
 
 def main():
     fname = "ml_uci.csv" 
-    out_dict, num_lines = load_csv_raw(fname)
-    print (f"num_lines: {num_lines}")
     outname = "tgbl-uci_edgelist.csv"
-    write2csv(outname, out_dict)
+    num_lines = load_write_csv(fname, outname)
+    print ("processed ", num_lines, "lines")
 
-    
 
 
 if __name__ == "__main__":
